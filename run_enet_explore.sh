0562546b79aaf27df95c2e83a4b7519a6786fe14
nicologhielmetti <nicolo.ghielmetti@gmail.com>
2021-11-08 11:55:29 +0100

    Minor fixes

--- run_enet_explore.sh
+++ run_enet_explore.sh
@@ -28,8 +28,8 @@ while getopts "r:f:c:q:p:i:o:" opt; do
   esac
 done
 
-# source /home/hls4ml/.bashrc
-# conda activate hls4ml-testing
+source /home/hls4ml/.bashrc
+conda activate hls4ml-testing
 pip uninstall hls4ml
 pip install git+https://github.com/nicologhielmetti/hls4ml.git@enet#egg=hls4ml[profiling]
 pip install keras
