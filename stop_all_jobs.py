258de1496f6f26373c0b0c6e2aaeb32267044f00
nicologhielmetti <nicolo.ghielmetti@gmail.com>
2021-11-22 17:13:36 +0100

    Added run2 scripts - minor fix

--- stop_all_jobs.py
+++ stop_all_jobs.py
@@ -1,5 +1,5 @@
 import os
-os.chdir('jobs')
+os.chdir('jobs_run2')
 jobs = os.listdir('.')
 for job in jobs:
     os.system('nomad job stop -purge -yes \'' + job.replace('job', 'scan').replace('.hcl', '') + '\'' )
