fccbcc08a5aad759350ce846470b75ffaeb1a086
nicologhielmetti <nicolo.ghielmetti@gmail.com>
2021-11-23 11:16:24 +0100

    Added run2 scripts - update to a restricted set of 14 models

--- generate-explore-enet-run2.py
+++ generate-explore-enet-run2.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas
 import yaml
 
@@ -5,31 +7,21 @@ import yaml
 import itertools
 import pandas as pd
 
-with open(r'explore_run2.yml') as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
+for config_file in os.listdir('explore_run2_scripts'):
+    with open('explore_run2_scripts/' + config_file, 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
 
-df = pd.read_csv('dataframe.csv')
-df = df[df['MaxLatency'] == df['MaxLatency'].min()][['Filters', 'Quantization', 'Precision']].drop_duplicates()
+    expl_config = config['explore']
+    expl_list = [v for k,v in expl_config.items()]
+    combinations = list(itertools.product(*expl_list))
 
-expl_config = config['explore']
-expl_list = [v for k,v in expl_config.items()]
-combinations = list(itertools.product(*expl_list))
+    with open('job.hcl') as f:
+        lines = f.read()
 
-with open('job.hcl') as f:
-    lines = f.read()
-
-for _, row in df.iterrows():
     for c in combinations:
-        reuse_factor = c[0]
-        n_filters = row['Filters']
-        clock_period = c[1]
-        quantization = row['Quantization']
-        precision = 'ap_fixed<{}>'.format(row['Precision'])
-        job = lines.format(reuse_factor=reuse_factor, n_filters=n_filters, clock_period=clock_period,
-                           quantization=quantization, precision=precision,
+        job = lines.format(reuse_factor=c[0], n_filters=c[1], clock_period=c[2], quantization=c[3], precision=c[4],
                            input_data=config['simulation']['input_data'],
                            output_predictions=config['simulation']['output_predictions'])
-        f = open('jobs_run2/job_f{}_clk{}_rf{}_q{}_{}.hcl'.format(n_filters, clock_period, reuse_factor, quantization,
-                                                                  precision), "w")
+        f = open('jobs_run2/job_f{}_clk{}_rf{}_q{}_{}.hcl'.format(c[1], c[2], c[0], c[3], c[4]), "w")
         f.write(job)
         f.close()
