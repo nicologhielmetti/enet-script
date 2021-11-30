76253205b8b52c43af334b0174fcc78469726e40
nicologhielmetti <nicolo.ghielmetti@gmail.com>
2021-11-30 12:28:00 +0100

    Added script to analyze results from synthesis + update of profiling script + new data pickles

---
+++ analyze_results.py
@@ -0,0 +1,121 @@
+import hls4ml
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import tarfile
+import shutil
+
+PARSE = False
+data = []
+data_path = 'data_pickles/data6.pkl'
+saved_dir = os.getcwd()
+if PARSE:
+    df = pd.read_pickle(data_path)
+    os.chdir('/eos/home-n/nghielme/')
+    ids = df['ID'].tolist()
+    for dir in os.listdir('.'):
+        if dir.startswith('enet-results-run'):
+            os.chdir(dir)
+        else:
+            continue
+        for model in os.listdir('.'):
+            datum = {}
+            if model.endswith('.tar.gz') and model[8:-7] not in ids:
+                with tarfile.open(model) as tar:
+                    subdir_and_files = [
+                        tarinfo for tarinfo in tar.getmembers()
+                        if tarinfo.name.startswith('hls')
+                    ]
+                    tar.extractall(members=subdir_and_files)
+            else:
+                continue
+
+            model = model[8:-7]
+            parsed = hls4ml.report.vivado_report.parse_vivado_report(model + '_FIFO_OPT')
+            shutil.rmtree(model + '_FIFO_OPT')
+            model_info = model.split('_')
+            datum['ID'] = model
+            datum['Run'] = dir.split('-')[-1]
+            datum['Filters'] = int(model_info[1][1:])
+            datum['Clock'] = int(model_info[2][3:])
+            datum['ReuseFactor'] = int(model_info[3][2:])
+            datum['Model'] = 'Clock: ' + str(datum['Clock']) + ' \n RF: ' + str(datum['ReuseFactor'])
+            datum['Quantization'] = int(model_info[4][1:])
+            datum['Precision'] = model_info[7].replace('-', ',')
+            try:
+                datum['LUTs%'] = int(round(parsed['ImplementationReport']['TotLUTs%']))
+                datum['FFs%'] = int(round(parsed['ImplementationReport']['FFs%']))
+                datum['RAM36Bs%'] = int(round(parsed['ImplementationReport']['RAMB36s%']))
+                datum['RAM18s%'] = int(round(parsed['ImplementationReport']['RAMB18s%']))
+                datum['DSPs%'] = int(round(parsed['ImplementationReport']['DSPs%']))
+                datum['WNS'] = parsed['TimingReport']['WNS']
+            except KeyError:
+                datum['LUTs%'] = 'NA'
+                datum['FFs%'] = 'NA'
+                datum['RAM36Bs%'] = 'NA'
+                datum['RAM18s%'] = 'NA'
+                datum['DSPs%'] = 'NA'
+                datum['WNS'] = 'NA'
+            datum['MaxLatency'] = parsed['CosimReport']['LatencyMax']
+            data.append(datum)
+        os.chdir('..')
+    os.chdir(saved_dir)
+    df1 = pd.DataFrame(data)
+    list_df = [df, df1]
+    res = df.concat(list_df)
+    res.to_pickle(data_path)
+else:
+    df = pd.read_pickle(data_path)
+
+df_na = df[df['LUTs%'] == 'NA']
+df_na.to_csv('NA_models.csv')
+df = df[df['LUTs%'] != 'NA']
+
+df['Max Latency [ms]'] = df['MaxLatency'] * 1e-5
+df['10 x WNS [ns]'] = df['WNS'] * 10
+df['Latency Overclock [ms]'] = df['MaxLatency'] * (10-df['WNS']) * 1e-6
+# df.to_csv('dataframe.csv')
+ap_fixed_16_6_data = df[df['Precision'] == '16,6']
+ap_fixed_8_4_data = df[df['Precision'] == '8,4']
+
+ap_fixed_8_4_data = ap_fixed_8_4_data.sort_values(by=['Clock', 'ReuseFactor'], ascending=True)
+ap_fixed_16_6_data = ap_fixed_16_6_data.sort_values(by=['Clock', 'ReuseFactor'], ascending=True)
+
+
+def print_plot(data, title):
+    def pointplot_with_outliers(*args, **kwargs):
+        local_data = kwargs.pop('data')
+        gt100ms = local_data.copy()
+        gt100ms.loc[gt100ms['Max Latency [ms]'] >= 100, 'Max Latency [ms]'] = 100
+        gt100ms[['LUTs%', 'FFs%', 'RAM36Bs%', 'RAM18s%', 'DSPs%', '10 x WNS [ns]', 'Latency Overclock [ms]']] = -10
+        lt100ms = local_data.copy()
+        lt100ms.loc[lt100ms['Max Latency [ms]'] >= 100, 'Max Latency [ms]'] = -10
+        gt100ms = gt100ms.melt(id_vars=['Model', 'ReuseFactor', 'Clock', 'Filters', 'Quantization'],
+                               value_vars=['LUTs%', 'FFs%', 'RAM36Bs%', 'RAM18s%', 'DSPs%',
+                                           'Max Latency [ms]', '10 x WNS [ns]', 'Latency Overclock [ms]'])
+        lt100ms = lt100ms.melt(id_vars=['Model', 'ReuseFactor', 'Clock', 'Filters', 'Quantization'],
+                               value_vars=['LUTs%', 'FFs%', 'RAM36Bs%', 'RAM18s%', 'DSPs%',
+                                           'Max Latency [ms]', '10 x WNS [ns]', 'Latency Overclock [ms]'])
+        palette = kwargs['palette']
+        if len(gt100ms) > 0:
+            kwargs['palette'] = 'dark:brown'
+            sns.pointplot(**kwargs, data=gt100ms, markers='x')
+            kwargs['palette'] = palette
+
+        sns.pointplot(**kwargs, data=lt100ms)
+
+
+    sns.set_theme()
+
+    g = sns.FacetGrid(data, col='Filters', row='Quantization', sharex=False, sharey=False, aspect=3.2,
+                      ylim=(0, 110))
+    g.map_dataframe(pointplot_with_outliers, join=False, x='Model', y='value', hue='variable', palette='tab10')
+    g.add_legend()
+    g.set_xticklabels(rotation=45)
+    g.fig.suptitle(title)
+    plt.show()
+
+
+print_plot(ap_fixed_8_4_data, 'Default Quantization: ap_fixed<8,4>')
+print_plot(ap_fixed_16_6_data, 'Default Quantization: ap_fixed<16,6>')
--- profile.py
+++ profile.py
@@ -1,25 +1,114 @@
-from hls4ml.converters import convert_from_config, parse_yaml_config
+import pandas as pd
+from hls4ml.model import HLSModel
 from hls4ml.model.profiling import numerical
+from hls4ml.utils import config_from_keras_model
+import hls4ml
+from hls4ml.converters import convert_from_keras_model
 import numpy as np
-from qkeras.utils import load_qmodel
+import qkeras
 import matplotlib.pyplot as plt
+
+from hls4ml.model.optimizer import OptimizerPass
+
+
+class TypeMatching(OptimizerPass):
+    def match(self, node):
+        is_match = (node.__class__.__name__ == 'Resize' and
+                    node.get_input_node().__class__.__name__ == 'Activation') or \
+                   (node.__class__.__name__ == 'Concat' and node.get_input_node() is not None)
+        return is_match
+
+    def transform(self, model, node):
+        inode = node.get_input_node()
+        in_out_type = inode.get_output_variable().type.precision
+        out_out_var = node.get_output_variable()
+        out_out_var.type.precision = in_out_type
+        return False
+
+
+hls4ml.model.optimizer.register_pass('type_matching', TypeMatching)
+
+print_plots = False
+print_numerical = True
+print_numerical_x = False
 
 hls_path = 'results_run1/results_hls_f8_clk7_rf10_q8_ap_fixed_16-6_/hls_f8_clk7_rf10_q8_ap_fixed_16-6__FIFO_OPT'
 keras_path = 'models_h5/hom8_32_8_8_8_8_8.h5'
 X = np.load('X_test.npy')
-config = parse_yaml_config(hls_path + '/hls4ml_config.yml')
-half_model = len(config['HLSConfig']['LayerName'].keys()) / 3
-i = 0
-for layer in config['HLSConfig']['LayerName'].keys():
-    if i < half_model:
-        config['HLSConfig']['LayerName'][layer]['Trace'] = True
-        i += 1
+
+keras_model = qkeras.utils.load_qmodel(keras_path, compile=False)
+
+act_blocks_ymodel = np.load('session_savings/act_blocks_ymodel.npy', allow_pickle=True).tolist()
+ymodel = {}
+for d in act_blocks_ymodel:
+    for k, v in d.items():
+        ymodel[k] = v
+
+hls4ml.model.optimizer.OutputRoundingSaturationMode.layers = ['Activation']
+hls4ml.model.optimizer.OutputRoundingSaturationMode.rounding_mode = 'AP_RND'
+hls4ml.model.optimizer.OutputRoundingSaturationMode.saturation_mode = 'AP_SAT'
+
+config_def = config_from_keras_model(keras_model, granularity='name',
+                                     default_precision='ap_fixed<16,6>',
+                                     default_reuse_factor=10)
+for layer in config_def['LayerName'].keys():
+    config_def['LayerName'][layer]['Trace'] = True
+config_def['LayerName']['input_1']['Precision'] = {'result': 'ap_ufixed<10,1>'}
+config_def['LayerName']['conv_initial']['Precision'] = {
+    'weight': 'ap_fixed<10,2>',
+    'bias': 'ap_fixed<16,6>',
+    'result': 'ap_fixed<16,4>',
+    'default': 'ap_fixed<16,6>',
+    'accum': 'ap_fixed<21,4>'
+}
+# config_def['LayerName']['batch_normalization']['Precision'] = {
+#                                                                 'accum': 'ap_fixed<20,6>',
+#                                                                 'scale': 'ap_fixed<16,6>',
+#                                                                 'bias': 'ap_fixed<16,6>'
+#                                                               }
+
+hls_model = convert_from_keras_model(keras_model,
+                                     output_dir='hls_f8_clk7_rf10_q8_ap_fixed_16-6__FIFO_OPT',
+                                     backend='VivadoAccelerator', io_type='io_stream',
+                                     board='zcu102', clock_period=7, hls_config=config_def)
+
+hls_model.write()
+_, ysim = hls_model.trace(X)
+
+act_blocks_ysim = []
+
+single_block_ysim = {}
+for k, v in ysim.items():
+    single_block_ysim[k] = v
+    if 're_lu' in k:
+        act_blocks_ysim.append(single_block_ysim)
+        single_block_ysim = {}
+
+df = pd.DataFrame()
+block = 0
+start = 0 if block == 0 else sum([len(e) for e in act_blocks_ysim[:block]])
+stop = sum([len(e) for e in act_blocks_ysim[:block + 1]])
+for layer in act_blocks_ysim[block].keys():
+    df[layer + '_csim'] = pd.Series(ysim[layer].flatten())
+    df[layer + '_qkeras'] = pd.Series(ymodel[layer].flatten())
+    print('Max difference for layer       {}: {}'.format(layer,
+                                                         np.abs(ysim[layer].flatten() - ymodel[layer].flatten()).max()))
+    print('min/MAX values range for layer {}: {} - {}'.format(layer, ymodel[layer].flatten().min(),
+                                                              ymodel[layer].flatten().max()))
+    if not print_plots:
+        continue
+    plt.figure()
+    plt.scatter(ysim[layer].flatten(), ymodel[layer].flatten(), s=0.2)
+    min_x = min(np.amin(ysim[layer]), np.amin(ymodel[layer]))
+    max_x = max(np.amax(ysim[layer]), np.amax(ymodel[layer]))
+    plt.plot([min_x, max_x], [min_x, max_x], c='gray')
+    plt.xlabel('hls4ml {}'.format(layer))
+    plt.ylabel('QKeras {}'.format(layer))
+    plt.show()
+
+if print_numerical:
+    if print_numerical_x:
+        wp, wph, ap, aph = numerical(model=keras_model, hls_model=hls_model, X=X, start=start, stop=stop)
     else:
-        break
-hls_model = convert_from_config(config)
-# hls_model.compile()
-keras_model = load_qmodel(keras_path, compile=False)
-wp, wph, ap, aph = numerical(model=keras_model, hls_model=hls_model, X=X)
-plt.show()
-# y_hls = hls_model.predict(X)
-# np.save(hls_path + '/y_hls.npy', y_hls)
+        wp, wph, ap, aph = numerical(model=keras_model, hls_model=hls_model, start=start, stop=stop)
+    plt.show()
