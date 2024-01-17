907edc23b6471f9a619e60b309a56383d41db2d6
Stelios Tzelepis <stel.tze09@gmail.com>
2024-01-17 17:05:58 +0100

    Fix generate hls script by including example

--- generate_y_hls.py
+++ generate_y_hls.py
@@ -1,6 +1,6 @@
 import hls4ml
 import numpy as np
-hls4ml_model_path = '.'
+hls4ml_model_path = './hls4ml_example_output'
 hls_model = hls4ml.converters.convert_from_config(hls4ml_model_path + '/hls4ml_config.yml')
 hls_model.compile()
 X = np.load('./data_pickles/X_test.npy')
