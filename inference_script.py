6184add70c8bae5c9fb8ada1c81512f3723fcf6a
Stelios Tzelepis <stel.tze09@gmail.com>
2024-01-22 15:12:10 +0100

    Change inference script

--- inference_script.py
+++ inference_script.py
@@ -1,40 +1,10 @@
-# %%
 import numpy as np
-import cv2
 from qkeras.utils import load_qmodel
-import hls4ml
-import model_under_test
-
-# %%
-X = np.load("data_pickles/X_test.npy")
-
-# %%
-from matplotlib import pyplot as plt
-
-image = cv2.cvtColor(X[2, :, :, :], cv2.COLOR_BGR2RGB)
-plt.imshow(image)
-plt.show()
+import cv2
+import matplotlib.pyplot as plt
+import plotting
 
 
-# %%
-def produce_hls_and_keras_model_outputs(model_inputs, path_to_keras_model):
-    # hls_model = hls4ml.converters.convert_from_config(
-    #     path_to_hls4ml_config_yml + "/hls4ml_config.yml"
-    # )
-    hls_model, keras_model, _ = model_under_test.get_hls_and_keras_models(path_to_keras_model, "ap_fixed<8,4>", 8, 7, "hls4ml_example_output0", False)
-
-    # hls_model.compile()
-    return hls_model.predict(model_inputs), keras_model(model_inputs)
-
-
-# def produce_keras_model_output(model_inputs, path_to_keras_model, keras_model_name):
-#     keras_model = load_qmodel(
-#         path_to_keras_model + "/" + keras_model_name, compile=False
-#     )
-#     return keras_model(model_inputs)
-
-
-# %%
 def plot_segmented_image(model_ouputs, image_to_plot):
     model_ouputs_indices = np.argmax(model_ouputs, axis=-1)
     model_ouputs = model_ouputs.astype(np.uint8)
@@ -56,16 +26,17 @@ def plot_segmented_image(model_ouputs, image_to_plot):
     plt.colorbar()
     plt.show()
 
+X = np.load('./data_pickles/X_test.npy')
+# keras_model = load_qmodel('hls4ml_example_output/keras_model.h5', compile=False)
+# y_keras = keras_model(X)
 
-# %%
-## Keras Model Inference
-
-y_hls, y_keras = produce_hls_and_keras_model_outputs(X, "models_h5_run2/hom4_32_16_16_16_16_16.h5")
-
-# %%
 # plot_segmented_image(np.array(y_keras), 2)
 
-# %%
-## hls4ml Model Inference
+import model_under_test
 
-# %%
+hls_model, keras_model, _ = model_under_test.get_hls_and_keras_models('hls4ml_example_output/keras_model.h5', 'ap_fixed<8,4>', 5, 7, 'model_3', False)
+
+#y_qkeras = keras_model.predict(X)
+#y_hls = hls_model.predict(X)
+#np.save('model_3/y_qkeras.npy', y_qkeras)
+#np.save('model_3/y_hls.npy', y_hls)
